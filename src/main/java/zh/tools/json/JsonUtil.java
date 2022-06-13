edb525c761316d0deb032b2e34dfb9fc4a26fc32
zhao <zhao@yicksolutions.com>
2022-06-13 13:32:27 +0800

    feat: init

---
+++ src/main/java/zh/tools/json/JsonUtil.java
@@ -0,0 +1,16 @@
+package zh.tools.json;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonUtil {
+    public static <T> JSONObject toJsonObject(T summary) {
+        return JSONObject.parseObject(JSONObject.toJSONString(summary));
+    }
+
+    public static <T> JSONArray toJsonArray(T data) {
+        return JSONArray.parseArray(JSON.toJSONString(data));
+    }
+
+}
