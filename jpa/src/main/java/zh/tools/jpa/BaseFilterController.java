63b9a76c5ab1d931e3a060edcfebd8d2b31bb655
p-nickzhang-q <13915426340@163.com>
2023-06-07 16:07:27 +0800

    feat: ✨ 0.0.6 update BaseFilterController

--- jpa/src/main/java/zh/tools/jpa/BaseFilterController.java
+++ jpa/src/main/java/zh/tools/jpa/BaseFilterController.java
@@ -8,48 +8,48 @@ import zh.tools.common.map.FilterMap;
 import java.io.Serializable;
 import java.util.List;
 
-public abstract class BaseFilterController<T, ID extends Serializable> {
-    public abstract BaseFilterService<T, ID> getService();
+public interface BaseFilterController<T, ID extends Serializable> {
+    BaseFilterService<T, ID> getService();
 
     @PostMapping
-    public T save(@RequestBody T t) {
+    default T save(@RequestBody T t) {
         return getService()
                 .save(t);
     }
 
     @PostMapping("/bulk")
-    public Iterable<T> save(@RequestBody List<T> list) {
+    default Iterable<T> save(@RequestBody List<T> list) {
         return getService()
                 .save(list);
     }
 
     @DeleteMapping
-    public void remove(@RequestParam List<ID> ids) {
+    default void remove(@RequestParam List<ID> ids) {
         getService()
                 .removeByIds(ids);
     }
 
     @GetMapping("/{id}")
-    public T findById(@PathVariable ID id) {
+    default T findById(@PathVariable ID id) {
         return getService()
                 .findById(id)
                 .orElseThrow(() -> new ValidateException("Id not found"));
     }
 
     @PostMapping("/filter/all")
-    public List<T> filterAll(@RequestBody FilterMap<String, Object> filterMap) {
+    default List<T> filterAll(@RequestBody FilterMap<String, Object> filterMap) {
         return getService()
                 .filter(filterMap);
     }
 
     @PostMapping("/filter")
-    public Page<T> filter(@RequestBody JpaFilterRequest filterRequest) {
+    default Page<T> filter(@RequestBody JpaFilterRequest filterRequest) {
         return getService()
                 .filter(filterRequest);
     }
 
     @PostMapping("/count")
-    public Long count(@RequestBody FilterMap<String, Object> filterMap) {
+    default Long count(@RequestBody FilterMap<String, Object> filterMap) {
         return getService().count(filterMap);
     }
 }
