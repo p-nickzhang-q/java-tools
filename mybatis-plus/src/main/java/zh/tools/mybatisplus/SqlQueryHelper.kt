2b3f26e164122e6beaa9e4c95de62a2357e9b48a
p-nickzhang-q <13915426340@163.com>
2025-04-30 16:15:49 +0800

    feat: add SqlQueryHelper for mybatis-plus

---
+++ mybatis-plus/src/main/java/zh/tools/mybatisplus/SqlQueryHelper.kt
@@ -0,0 +1,44 @@
+package zh.tools.mybatisplus
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
+import com.baomidou.mybatisplus.core.mapper.BaseMapper
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
+import zh.tools.maptosql.JsonToSqlConverter
+import zh.tools.maptosql.QueryBuilder
+import zh.tools.maptosql.json
+
+class SqlQueryHelper(
+    private val jsonToSqlConverter: JsonToSqlConverter,
+    private val className: String
+) {
+    fun <M : BaseMapper<T>, T>
+            ServiceImpl<M, T>.listBySql(block: QueryBuilder.() -> Unit): List<T> {
+        val sql = jsonToSqlConverter.convertToWhereClause(className, json(block))
+        return query().apply(sql).list().toList()
+    }
+
+    fun <M : BaseMapper<T>, T>
+            ServiceImpl<M, T>.getOneBySql(block: QueryBuilder.() -> Unit): T? {
+        val sql = jsonToSqlConverter.convertToWhereClause(className, json(block))
+        return query().apply(sql).list().firstOrNull()
+    }
+
+    fun <M : BaseMapper<T>, T>
+            ServiceImpl<M, T>.listBySql(json: Map<String, Any>): List<T> {
+        val sql = jsonToSqlConverter.convertToWhereClause(className, json)
+        return query().apply(sql).list().toList()
+    }
+
+    fun <M : BaseMapper<T>, T>
+            ServiceImpl<M, T>.pageBySql(json: Map<String, Any>, page: Long, pageSize: Long): Page<T> {
+        val sql = jsonToSqlConverter.convertToWhereClause(className, json)
+        val pageObj = Page<T>(page, pageSize)
+        val queryWrapper = if (sql.isNotEmpty()) {
+            QueryWrapper<T>().apply(sql)
+        } else {
+            QueryWrapper()
+        }
+        return page(pageObj, queryWrapper)
+    }
+}
